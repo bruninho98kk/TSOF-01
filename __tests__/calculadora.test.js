40dc7d4cb5a6058382353d83be6843bbf4ee4512
Bruno Quintino <brunoquintino.tds2.senai@gmail.com>
2025-08-26 11:13:53 -0300

    feat: create multiplicar e dividir

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,21 +1,61 @@
-import { soma, subtrai } from "../src/calculadora.js";
+import { somar, subtrair, dividir, multiplicar } from "../src/calculadora.js";
 
 describe("Calculadora", () => {
-    test("Deve somar dois números corretamente", () => {
-       const a = 2;
-       const b = 1;
+  test("Deve somar dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+    const a = 2;
+    const b = 3;
 
-       const resultado = soma(a, b);
+    // Act (Executar)
+    const resultado = somar(a, b);
 
-       expect(resultado).toBe(3);
-    });
+    // Assert (Verificar)
+    expect(resultado).toBe(5);
+  });
 
-    test("Deve subtrair dois números corretamente", () => {
-        const a = 5;
-        const b = 2;
+  test("Deve subtrair dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+    const a = 13;
+    const b = 3;
 
-        const resultado = subtrai(a, b);
+    // Act (Executar)
+    const resultado = subtrair(a, b);
 
-        expect(resultado).toBe(3);
-    });
-})
+    // Assert (Verificar)
+    expect(resultado).toBe(10);
+  });
+
+  test("Deve multiplicar dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+    const a = 10;
+    const b = 4;
+
+    // Act (Executar)
+    const resultado = multiplicar(a, b);
+
+    // Assert (Verificar)
+    expect(resultado).toBe(40);
+  });
+
+  test("Deve dividir dois números corretamente", () => {
+    // AAA
+    // Arrange (Preparar)
+    const a = 10;
+    const b = 5;
+
+    // Act (Executar)
+    const resultado = dividir(a, b);
+
+    // Assert (Verificar)
+    expect(resultado).toBe(2);
+  });
+
+  test("Não é possível dividir por zero", () => {
+    expect(() => {
+      dividir(10, 0);
+    }).toThrow("Não é possível dividir por zero");
+  });
+});
--- src/calculadora.js
+++ src/calculadora.js
@@ -1,7 +1,20 @@
-export function soma(a, b) {
-  return a + b;
-}
-
-export function subtrai(a, b) {
+// Função simples para testar soma
+export function somar(a, b) {
+    return a + b;
+  }
+  
+  export function subtrair(a, b) {
     return a - b;
-}
+  }
+  
+  export function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  export function dividir(a, b) {
+    if (b == 0) {
+      throw new Error("Não é possível dividir por zero");
+    }
+  
+    return a / b;
+  }
